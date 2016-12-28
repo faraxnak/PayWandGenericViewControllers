1505b5819e0d696c6cc283f9a009cba2f4707d9f
faraxnak <farid@farids-macbook-pro.local>
2016-12-28 18:23:25 +0330

    Initial Commit

---
+++ PayWandGenericViewControllers/PayWandGenericViewControllers.h
@@ -0,0 +1,19 @@
+//
+//  PayWandGenericViewControllers.h
+//  PayWandGenericViewControllers
+//
+//  Created by Farid on 12/28/16.
+//  Copyright © 2016 ParsPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PayWandGenericViewControllers.
+FOUNDATION_EXPORT double PayWandGenericViewControllersVersionNumber;
+
+//! Project version string for PayWandGenericViewControllers.
+FOUNDATION_EXPORT const unsigned char PayWandGenericViewControllersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayWandGenericViewControllers/PublicHeader.h>
+
+
